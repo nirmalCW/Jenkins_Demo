59e3ab8a1a584d955490503abd3531e20ed0bb4c
Krunal <krunal.chauhan@capitaworld.com>
2018-10-09 12:15:42 +0530

    This is new project

---
+++ src/main/java/com/ls/jenkins/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.ls.jenkins;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("This is hello world class >>>>>>>>>>>>>>>>>>>>");
+    }
+}
